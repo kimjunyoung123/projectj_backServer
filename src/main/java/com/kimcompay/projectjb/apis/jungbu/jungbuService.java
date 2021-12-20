43acfd04ade1e1c80bb0ce8d8f957c9be04568a2
novb1492 <novb1492@naver.com>
2021-12-21 00:36:18 +0900

    정부 api호출성공

--- src/main/java/com/kimcompay/projectjb/apis/jungbu/jungbuService.java
+++ src/main/java/com/kimcompay/projectjb/apis/jungbu/jungbuService.java
@@ -1,5 +1,6 @@
 package com.kimcompay.projectjb.apis.jungbu;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +12,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 @Service
 public class jungbuService {
@@ -23,9 +29,9 @@ public class jungbuService {
     @Autowired
     private requestTo requestTo;
 
-    public JSONObject getCompanyNum(int compay_num) {
+    public void getCompanyNum(int compay_num) {
         logger.info("getCompanyNum");
-        //body생성
+       /* //body생성
         JSONObject body=new JSONObject();
         //사업자등록증 담기
         List<String>integers=new ArrayList<>();
@@ -36,6 +42,37 @@ public class jungbuService {
         //헤더담기
         HttpHeaders headers=new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        return requestTo.requestPost(body, url, headers);
+        return requestTo.requestPost(body, url, headers);*/
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType,
+                "{\n  \"businesses\": " +
+                        "   [\n    " +
+                        "       {\n      " +
+                        "           \"b_no\": \"0000000000\",\n      " +
+                        "           \"start_dt\": \"20000101\",\n      " +
+                        "           \"p_nm\": \"홍길동\",\n      " +
+                        "           \"p_nm2\": \"홍길동\",\n      " +
+                        "           \"b_nm\": \"(주)테스트\",\n      " +
+                        "           \"corp_no\": \"0000000000000\",\n      " +
+                        "           \"b_sector\": \"\",\n      " +
+                        "           \"b_type\": \"\"\n    " +
+                        "       }\n  " +
+                        "   ]\n" +
+                        "}");
+        Request request = new Request.Builder()
+                .url("http://api.odcloud.kr/api/nts-businessman/v1/validate?serviceKey="+apikey)
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        Response response;
+        try {
+                response = client.newCall(request).execute();
+                System.out.println(response.body().string());
+        } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
     }
 }
