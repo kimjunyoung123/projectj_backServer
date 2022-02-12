ca2197dd58b78cdd2bf6fb22f88c855b3cf7e446
novb1492 <novb1492@naver.com>
2022-02-12 21:17:22 +0900

    그냥 가격으로 받아서 db에 넣을때 2개로 나눠 넣던가 어쩃든 숫자만 받자

--- src/main/java/com/kimcompay/projectjb/users/company/productService.java
+++ src/main/java/com/kimcompay/projectjb/users/company/productService.java
@@ -17,16 +17,49 @@ public class productService {
 
     public JSONObject insert(tryProductInsertDto tryProductInsertDto) {
         logger.info("insert");
-        checkEvent(tryProductInsertDto.getEventFlag(), tryProductInsertDto.getEventInfors());
+        checkValues(tryProductInsertDto.getPrice(),tryProductInsertDto.getEventFlag(), tryProductInsertDto.getEventInfors());
+        if(tryProductInsertDto.getEventFlag()){
+            logger.info("이벤트 테이블 insert시도");
+
+        }
         return null;
+    }
+    private void checkValues(String price,boolean eventFlag,List<Map<String,Object>>eventInfors) {
+        logger.info("checkValue");
+        System.out.println(price.length()+"길이~");
+        //가격유효성검사
+        if(checkPrice(price)){
+            throw utillService.makeRuntimeEX("가격이 0원입니다", "checkValue");
+        }
+        checkEvent(eventFlag, eventInfors);
+
+    }
+    private boolean checkPrice(String price) {
+        logger.info("checkPrice");
+        if(Integer.parseInt(price.replace(",", ""))<=0){
+            return true;
+        }else if(price.length()>3&&!price.contains(",")){
+            throw utillService.makeRuntimeEX("1,000원이상은 쉼표로 구분해주세요", "checkValue");
+        }else if(price.contains(",")){
+            
+            char[] c=price.toCharArray();
+            for(int i=0;i<c.length;i++){
+                int cn=(int)c[i];
+                if(cn<48||cn>57){
+                    throw utillService.makeRuntimeEX("가격형식이 올바르지 않습니다", "checkValue");
+                }
+            }
+        }
+        return false;
     }
     private void checkEvent(boolean eventFlag,List<Map<String,Object>>eventInfors) {
         logger.info("checkEvent");
         if(eventFlag){
             logger.info("이벤트가 있는 제품");
             for(Map<String,Object>eventInfor:eventInfors){
-                if(Integer.parseInt(eventInfor.get("price").toString().replace(",", ""))<=0){
+                if(checkPrice(eventInfor.get("price").toString())){
                     throw utillService.makeRuntimeEX("이벤트가격이 0원 보다 작거나 같습니다 \n 날짜: "+eventInfor.get("date"), "checkEvent");
+
                 }
             }
         }
