892ba60240214ff27de2db7c704e1e79f8786324
kimjunyoung <kimjy80@sesisoft.com>
2022-02-17 14:19:37 +0900

    aop 이름변경

--- src/main/java/com/kimcompay/projectjb/aops/aopService.java
+++ src/main/java/com/kimcompay/projectjb/aops/aopService.java
@@ -7,7 +7,6 @@ import com.kimcompay.projectjb.utillService;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +14,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Aspect
-public class logAop {
-    private Logger logger=LoggerFactory.getLogger(logAop.class); 
+public class aopService {
+    private Logger logger=LoggerFactory.getLogger(aopService.class); 
 
     @Around("execution(* com.kimcompay.projectjb.users.company.*.*(..))"
     +"||execution(* com.kimcompay.projectjb.users.user.*.*(..))"
