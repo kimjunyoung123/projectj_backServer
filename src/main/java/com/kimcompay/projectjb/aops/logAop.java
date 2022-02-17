8bb1bfe1ca566d90f43ca0c55e607fdeb27f2b53
kimjunyoung <kimjy80@sesisoft.com>
2022-02-17 09:59:22 +0900

    작업시작

--- src/main/java/com/kimcompay/projectjb/aops/logAop.java
+++ src/main/java/com/kimcompay/projectjb/aops/logAop.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,11 @@ public class logAop {
     private Logger logger=LoggerFactory.getLogger(logAop.class); 
 
     @Around("execution(* com.kimcompay.projectjb.users.company.flyerService.*(..))||execution(* com.kimcompay.projectjb.users.user.userService.*(..))")
-    public Object writeLog(ProceedingJoinPoint JoinPoint) throws Throwable {
+    public Object writeLog(ProceedingJoinPoint joinPoint) throws Throwable {
         logger.info("writeLog");
-        logger.info("요청변수: "+JoinPoint.getArgs());
-        return JoinPoint.proceed();   
+        MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+        logger.info("mehtod: "+signature.getName());
+        logger.info("요청변수: "+joinPoint.getArgs());
+        return joinPoint.proceed();   
     }
 }
