c2eecbd66366590a834477f28b9871ae7abd3ff5
kimjunyoung <kimjy80@sesisoft.com>
2022-02-17 10:14:16 +0900

    이제 줄바꿈해서 변수를 써야겠다

--- src/main/java/com/kimcompay/projectjb/aops/logAop.java
+++ src/main/java/com/kimcompay/projectjb/aops/logAop.java
@@ -1,6 +1,7 @@
 package com.kimcompay.projectjb.aops;
 
-import org.aspectj.lang.JoinPoint;
+import java.lang.reflect.Method;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,12 +16,23 @@ import org.springframework.stereotype.Service;
 public class logAop {
     private Logger logger=LoggerFactory.getLogger(logAop.class); 
 
-    @Around("execution(* com.kimcompay.projectjb.users.company.flyerService.*(..))||execution(* com.kimcompay.projectjb.users.user.userService.*(..))")
+    @Around("execution(* com.kimcompay.projectjb.users.company.flyerService.*(..))"
+    +"||execution(* com.kimcompay.projectjb.users.user.userService.*(..))"
+    +"||execution(* com.kimcompay.projectjb.users.company.storeService.*(..))"
+    +"||execution(* com.kimcompay.projectjb.users.company.productService.*(..))")
     public Object writeLog(ProceedingJoinPoint joinPoint) throws Throwable {
         logger.info("writeLog");
         MethodSignature signature = (MethodSignature)joinPoint.getSignature();
-        logger.info("mehtod: "+signature.getName());
-        logger.info("요청변수: "+joinPoint.getArgs());
+        Method method=signature.getMethod();
+        logger.info("mehtod: "+method.getName());
+        int len=method.getParameterCount();
+        String[] paramNames=signature.getParameterNames();
+        Object[] paramValues =joinPoint.getArgs();
+        for(int i=0;i<len;i++){
+            logger.info("요청변수: "+paramNames[i]+" 요청값: "+paramValues[i].toString());
+
+        }
+      
         return joinPoint.proceed();   
     }
 }
