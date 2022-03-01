3168891533fb1503ad3a9b71c209ca40e4cbcaa3
novb1492 <novb1492@naver.com>
2022-03-01 12:04:13 +0900

    가격을 같이줘야한다 잊어먹고 있었네 밥먹고 더손보자

--- src/main/java/com/kimcompay/projectjb/payments/basket/model/basketDao.java
+++ src/main/java/com/kimcompay/projectjb/payments/basket/model/basketDao.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface basketDao extends JpaRepository<basketVo,Integer> {
     
-    @Query(value = "select a.* ,(select count(*) from baskets where user_id=?)totalCount,b.product_name,b.product_img_path"
-    +" from baskets a inner join products b on a.product_id=b.product_id where a.user_id=? order by a.basket_id limit ?,?",nativeQuery = true)
+    @Query(value = "select * ,(select count(*) from baskets where user_id=?)totalCount"
+    +" from baskets where user_id=? order by basket_id limit ?,?",nativeQuery = true)
     List<Map<String,Object>>findByUserId(int userId,int sameUserId,int start,int pageSize);
 
 }
--- src/main/java/com/kimcompay/projectjb/payments/basket/service/basketService.java
+++ src/main/java/com/kimcompay/projectjb/payments/basket/service/basketService.java
@@ -1,5 +1,7 @@
 package com.kimcompay.projectjb.payments.basket.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +9,8 @@ import com.kimcompay.projectjb.utillService;
 import com.kimcompay.projectjb.payments.basket.model.basketDao;
 import com.kimcompay.projectjb.payments.basket.model.basketVo;
 import com.kimcompay.projectjb.payments.basket.model.tryInsertDto;
+import com.kimcompay.projectjb.users.company.model.products.productVo;
+import com.kimcompay.projectjb.users.company.service.productService;
 
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,8 @@ public class basketService {
 
     @Autowired
     private basketDao basketDao;
+    @Autowired
+    private productService productService;
 
     public JSONObject tryInsert(tryInsertDto tryInsertDto) {
         basketVo vo=basketVo.builder().count(tryInsertDto.getCount()).productId(tryInsertDto.getProductId()).userId(utillService.getLoginId()).build();
@@ -28,6 +34,16 @@ public class basketService {
         int userId=utillService.getLoginId();
         List<Map<String,Object>>baskets=basketDao.findByUserId(userId,userId, utillService.getStart(page, pageSize)-1, pageSize);
         utillService.checkDaoResult(baskets, "불러올 제품이 없습니다", "getBaskets");
+        //제품 이벤트 판단후 가격 계산
+        int size=baskets.size();
+        for(int i=0;i<size;i++){
+            Map<String,Object>basket=new HashMap<>();
+            productVo prouctVo=(productVo)productService.getProduct(Integer.parseInt(baskets.get(i).get("product_id").toString())).get("message");
+            basket.put("price", prouctVo.getPrice());
+            basket.put("product_name", prouctVo.getProductName());
+            basket.put("product_img_path", prouctVo.getProductImgPath());
+            baskets.set(i, basket);
+        }
         JSONObject response=new JSONObject();
         response.put("baskets", baskets);
         response.put("totalPage", utillService.getTotalPage(Integer.parseInt(baskets.get(0).get("totalCount").toString()), pageSize));
