2e4dab284f51af0481e0abfb72bd83084f6b91a2
novb1492 <novb1492@naver.com>
2022-03-19 23:34:06 +0900

    아씨 ㅋㅋㅋ order만 주면 됐었잖아

--- src/main/java/com/kimcompay/projectjb/payments/model/pay/paymentDao.java
+++ src/main/java/com/kimcompay/projectjb/payments/model/pay/paymentDao.java
@@ -10,13 +10,13 @@ import org.springframework.data.jpa.repository.Query;
 public interface paymentDao extends JpaRepository<paymentVo,Integer> {
     boolean existsByMchtTrdNo(String mchtTrdNo);
     
-    @Query(value = "select a.*,e.order_id,e.order_price,e.order_count,e.order_coupon,(select count(*) from payments where user_id=?)totalCount"
-    +" from payments a left join orders e on a.mcht_trd_no=e.order_mcht_trd_no"
-    +" where a.user_id=? order by a.payment_id desc limit ?,?",nativeQuery = true)
+    @Query(value = "select *,(select count(*) from orders where user_id=?)totalCount"
+    +" from orders"
+    +" where user_id=? order by order_id desc limit ?,?",nativeQuery = true)
     List<Map<String,Object>>findJoinCardVbankKpayOrder(int userId,int sameUserId,int start,int pageSize);
 
-    @Query(value = "select a.*,e.order_id,e.order_price,e.order_count,e.order_coupon,(select count(*) from payments where user_id=? and payment_created between ? and ?)totalCount"
-    +" from payments a left join orders e on a.mcht_trd_no=e.order_mcht_trd_no"
-    +" where a.user_id=? and a.payment_created between ? and ? order by a.payment_id desc limit ?,?",nativeQuery = true)
+    @Query(value = "select *,(select count(*) from orders where user_id=? and order_created between ? and ?)totalCount"
+    +" from orders"
+    +" where user_id=? and order_created between ? and ? order by order_id desc limit ?,?",nativeQuery = true)
     List<Map<String,Object>>findJoinCardVbankKpayOrder(int userId,Timestamp startDate,Timestamp endDate,int sameUserId,Timestamp sameStartDate,Timestamp sameEndDate,int start,int pageSize);
 }
