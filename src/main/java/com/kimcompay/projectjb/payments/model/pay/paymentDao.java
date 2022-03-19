961b6ec377665fa18b53d2178a8359545197f10f
novb1492 <novb1492@naver.com>
2022-03-19 23:28:05 +0900

    주문내역에서 클릭하면 결제정보 보여주자

--- src/main/java/com/kimcompay/projectjb/payments/model/pay/paymentDao.java
+++ src/main/java/com/kimcompay/projectjb/payments/model/pay/paymentDao.java
@@ -10,13 +10,13 @@ import org.springframework.data.jpa.repository.Query;
 public interface paymentDao extends JpaRepository<paymentVo,Integer> {
     boolean existsByMchtTrdNo(String mchtTrdNo);
     
-    @Query(value = "select a.*,b.*,c.*,d.*,e.order_id,e.order_price,e.order_count,e.order_coupon,(select count(*) from payments where user_id=?)totalCount"
-    +" from payments a left join vbanks b on a.mcht_trd_no=b.vbank_mcht_trd_no left join cards c on a.mcht_trd_no=c.card_mcht_trd_no left join kpays d on a.mcht_trd_no=kpay_mcht_trd_no left join orders e on a.mcht_trd_no=e.order_mcht_trd_no"
+    @Query(value = "select a.*,e.order_id,e.order_price,e.order_count,e.order_coupon,(select count(*) from payments where user_id=?)totalCount"
+    +" from payments a left join orders e on a.mcht_trd_no=e.order_mcht_trd_no"
     +" where a.user_id=? order by a.payment_id desc limit ?,?",nativeQuery = true)
     List<Map<String,Object>>findJoinCardVbankKpayOrder(int userId,int sameUserId,int start,int pageSize);
 
-    @Query(value = "select a.*,b.*,c.*,d.*,e.order_id,e.order_price,e.order_count,e.order_coupon,(select count(*) from payments where user_id=? and payment_created between ? and ?)totalCount"
-    +" from payments a left join vbanks b on a.mcht_trd_no=b.vbank_mcht_trd_no left join cards c on a.mcht_trd_no=c.card_mcht_trd_no left join kpays d on a.mcht_trd_no=kpay_mcht_trd_no left join orders e on a.mcht_trd_no=e.order_mcht_trd_no"
+    @Query(value = "select a.*,e.order_id,e.order_price,e.order_count,e.order_coupon,(select count(*) from payments where user_id=? and payment_created between ? and ?)totalCount"
+    +" from payments a left join orders e on a.mcht_trd_no=e.order_mcht_trd_no"
     +" where a.user_id=? and a.payment_created between ? and ? order by a.payment_id desc limit ?,?",nativeQuery = true)
     List<Map<String,Object>>findJoinCardVbankKpayOrder(int userId,Timestamp startDate,Timestamp endDate,int sameUserId,Timestamp sameStartDate,Timestamp sameEndDate,int start,int pageSize);
 }
