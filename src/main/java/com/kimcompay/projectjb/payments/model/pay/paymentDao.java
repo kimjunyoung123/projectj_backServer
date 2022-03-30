d61d7a02c46e399022c304e3902c37f11e14bdc9
kimjunyoung <kimjy80@sesisoft.com>
2022-03-30 15:50:12 +0900

    입금여부 표시까지

--- src/main/java/com/kimcompay/projectjb/payments/model/pay/paymentDao.java
+++ src/main/java/com/kimcompay/projectjb/payments/model/pay/paymentDao.java
@@ -14,9 +14,9 @@ public interface paymentDao extends JpaRepository<paymentVo,Integer> {
     +" where a.user_id=?",nativeQuery = true)
     List<Map<String,Object>>findJoinCardVbankKpayOrder(int userId);
 
-    @Query(value ="SELECT DISTINCT a.mcht_trd_no as ad,  a.*,"
+    @Query(value ="SELECT DISTINCT a.mcht_trd_no as ad,a.*,d.vbank_status,"
     +"(select count(DISTINCT a.payment_id) FROM payments a left join orders b on a.mcht_trd_no = b.order_mcht_trd_no and b.store_id=?)totalCount,c.user_email"
-    +" FROM payments a left join orders b on a.mcht_trd_no = b.order_mcht_trd_no and b.store_id=? left join users c on c.uid=b.user_id"
+    +" FROM payments a left join orders b on a.mcht_trd_no = b.order_mcht_trd_no and b.store_id=? left join users c on c.uid=b.user_id left join vbanks d on a.mcht_trd_no = d.vbank_mcht_trd_no "
     +" order by a.payment_id desc limit ?,?" ,nativeQuery =true)
     List<Map<String,Object>>findJoinByStoreId(int storeId,int sameStoreId,int start,int pageSize);
 }
